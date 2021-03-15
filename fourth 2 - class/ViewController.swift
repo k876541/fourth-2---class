c5ce91017189390656ed7a13da64815204b06b01
ryan_chang <k876541@hotmail.com>
2021-03-15 21:05:31 +0800

    Initial Commit

---
+++ fourth 2 - class/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fourth 2 - class
+//
+//  Created by Ryan Chang on 2021/3/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
